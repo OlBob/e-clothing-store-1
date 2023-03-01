fb17e9c50d4a2090275ac0a4175d972184aac8e6
Oleksandr <cerebrum94@gmail.com>
2023-03-01 18:04:15 +0200

    adding header component anf links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<HomePage />} />
-      <Route path='shop' element={<ShopPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='shop' element={<ShopPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown_logo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link to="shop" className="option">SHOP</Link>
+        <Link to="contacts" className="option">CONTACTS</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
